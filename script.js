e07e612a112299d4fc98f03f9cbed03fee2327eb
Adrian Justino Maestre <AJustino@aurensis.com>
2024-07-23 12:06:00 +0200

    Show all current dates

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", async function () {
   const URL = "./data/travel.json";
   const data = await fetchData(URL);
-  console.log(data)
+  console.log(data);
 
   document
     .getElementById("searchButton")
@@ -27,8 +27,7 @@ document.addEventListener("DOMContentLoaded", async function () {
       }
       const data = await response.json();
 
-      return data
-
+      return data;
     } catch (error) {
       console.error("Error:", error);
     }
@@ -36,13 +35,13 @@ document.addEventListener("DOMContentLoaded", async function () {
 
   function searchKeywords(input, data) {
     let results = [];
-    console.log(input.substring(0, input.length-1))
+    //if(input === "") return []
     for (let item in data) {
-        if(item.includes(input.substring(0, input.length-1))) {
-            data[item].forEach(element => {
-                results.push(element)
-            });
-        }
+      if (item.includes(input.substring(0, input.length - 1))) {
+        data[item].forEach((element) => {
+          results.push(element);
+        });
+      }
     }
 
     return results;
@@ -55,28 +54,52 @@ document.addEventListener("DOMContentLoaded", async function () {
       resultsContainer.innerHTML = "<p>No results found.</p>";
     } else {
       results.forEach((result) => {
-        console.log(result)
         const resultElement = document.createElement("div");
-        resultElement.className = "result";
-        resultElement.innerHTML = `<h3>${result.name}</h3>`;
 
-        if(!result.description) {
-            result.cities.forEach(element => {
-                showCurrentTime(element.name.split(" ")[0].split(",")[0]);
-                resultElement.innerHTML += `
-                        <h5>${element.name}</h5>
-                        <p>${element.description}</p>
-                        <img src="./images/${element.imageUrl}" alt="${element.name} image" id="imgPlace">
-                    `;
-            });
+        if (!result.description) {
+          result.cities.forEach((element) => {
+
+            const nombreElemento = element.name.split(" ")[0].split(",")[0]
+            const name = document.createElement("h3");
+            name.textContent = element.name;
+            resultsContainer.appendChild(name);
+
+            const timeDisplay = document.createElement("h6");
+            timeDisplay.className = "time-display";
+            timeDisplay.id = nombreElemento;
+            resultsContainer.appendChild(timeDisplay);
+            showCurrentTime(nombreElemento);
+
+            const description = document.createElement("p");
+            description.textContent = element.description;
+            resultsContainer.appendChild(description);
+
+            const image = document.createElement("img");
+            image.id = "imgPlace";
+            image.setAttribute("src", `./images/${element.imageUrl}`);
+            resultsContainer.appendChild(image);
+          });
         } else {
-            showCurrentTime(result.name.split(" ")[0].split(",")[0]);
-            resultElement.innerHTML += `
-                    <p>${result.description}</p>
-                    <img src="./images/${result.imageUrl}" alt="${result.name} image" id="imgPlace">
-                `;
+          const name = document.createElement("h3");
+          name.textContent = result.name;
+          resultsContainer.appendChild(name);
+
+          const timeDisplay = document.createElement("h6");
+            timeDisplay.className = "time-display";
+          timeDisplay.id = `${result.name.split(" ")[0].split(",")[0]}`;
+          resultsContainer.appendChild(timeDisplay);
+          showCurrentTime(result.name.split(" ")[0].split(",")[0]);
+
+          const description = document.createElement("p");
+          description.textContent = result.description;
+          resultsContainer.appendChild(description);
+
+          const image = document.createElement("img");
+          image.id = "imgPlace";
+          image.setAttribute("src", `./images/${result.imageUrl}`);
+          resultsContainer.appendChild(image);
         }
-        
+
         resultsContainer.appendChild(resultElement);
       });
     }
@@ -88,50 +111,53 @@ document.addEventListener("DOMContentLoaded", async function () {
   }
 });
 
-
 async function getTime(placeName) {
+    console.log("---->> " + placeName)
     const timeZones = {
-        "Sydney": "Australia/Sydney",
-        "Melbourne": "Australia/Melbourne",
-        "Tokyo": "Asia/Tokyo",
-        "Kyoto": "Asia/Tokyo",
-        "São Paulo": "America/Sao_Paulo",
-        "Angkor Wat": "Asia/Phnom_Penh",
-        "Taj Mahal": "Asia/Kolkata",
-        "Bora Bora": "Pacific/Tahiti",
-        "Copacabana": "America/Sao_Paulo"
+      Sydney: "Australia/Sydney",
+      Melbourne: "Australia/Melbourne",
+      Tokyo: "Asia/Tokyo",
+      Kyoto: "Asia/Tokyo",
+      "São Paulo": "America/Sao_Paulo",
+      Rio: "America/Sao_Paulo",
+      "Angkor Wat": "Asia/Phnom_Penh",
+      "Taj Mahal": "Asia/Kolkata",
+      "Bora Bora": "Pacific/Tahiti",
+      Copacabana: "America/Sao_Paulo",
     };
-
-    for (let place in timeZones) {
-        console.log("Lugar -> " + place + " incluye -> " + placeName + "???")
-        if (place.toLowerCase().includes(placeName.toLowerCase())) {
-            const timeZone = timeZones[place];
-            try {
-                const targetUrl = `https://timeapi.io/api/Time/current/zone?timeZone=${timeZone}`;
-                const response = await fetch(targetUrl);
-                const data = await response.json();
-                return data;
-            } catch (error) {
-                console.error("Error fetching time data:", error);
-                return "Error fetching time data";
-            }
-        }
+  
+    const normalizedPlaceName = placeName.toLowerCase();
+  
+    const matchingPlace = Object.keys(timeZones).find(place =>
+      place.toLowerCase().includes(normalizedPlaceName)
+    );
+  
+    if (matchingPlace) {
+      const timeZone = timeZones[matchingPlace];
+      const options = {
+        timeZone: timeZone,
+        hour12: false,
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+      };
+      const currentTime = new Date().toLocaleTimeString("en-US", options);
+      console.log(`Lugar -> ${matchingPlace} -> ${currentTime}`);
+      return currentTime;
     }
-    return "Time zone not found";
-}
+  
+    return "Time zone not found -> " + placeName;
+  }
+  
 
 function showCurrentTime(placeName) {
-    const timeDisplay = document.createElement("h6");
-    timeDisplay.className = "time-display"
+  async function updateTime() {
+    const timeDisplay = document.getElementById(placeName);
+    const currentTime = await getTime(placeName);
+    timeDisplay.textContent = `Current time in ${placeName}: ${currentTime}`;
+    console.log(timeDisplay.id + "- despues del inner - " + currentTime)
+  }
 
-    async function updateTime() {
-        const currentTime = await getTime(placeName);
-        timeDisplay.textContent = currentTime;
-        console.log(currentTime)
-    }
-
-    updateTime();
-    // setInterval(updateTime, 1000);
+  updateTime();
+  setInterval(updateTime, 1000);
 }
-
-
